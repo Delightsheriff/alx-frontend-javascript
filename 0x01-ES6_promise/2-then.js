29ea952962cf39c6f3c2526257178c963f9f1dd3
Delight Sheriff <delightsheriff@gmail.com>
2024-01-03 14:20:38 +0100

    2. Catch me if you can!-3

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
-export default function handleResponseFromAPI(promise) {
-  return promise
+export default function handleResponseFromAPI(promisedResponse) {
+  return promisedResponse
     .then(() => ({
       status: 200,
       body: 'Success',
     }))
-    .catch(() => new Error({}))
+    .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
