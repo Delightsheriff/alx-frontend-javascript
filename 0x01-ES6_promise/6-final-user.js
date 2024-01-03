6c39c3938cefa6e9b0be4ac308ab1797ad7f937f
Delight Sheriff <delightsheriff@gmail.com>
2024-01-03 20:13:44 +0100

    6. Handle multiple promises-1

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,8 +3,11 @@ import { uploadPhoto } from './utils';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.all([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((res) => {
-      console.log(`${res[0].firstName} ${res[0].lastName} ${res[1].body}`);
+    .then((results) => {
+      const [user, photo] = results;
+      return [user.firstName, user.lastName, photo.body];
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => {
+      throw new Error('Signup system offline'); // Rethrow for proper error handling
+    });
 }
