615056e0e6895a424a47d541029a246b08eda70b
Delight Sheriff <delightsheriff@gmail.com>
2024-01-04 20:55:05 +0100

    4. Pricing-2

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,5 +1,33 @@
-import Currency from './3-currency';
+// export default class Pricing {
+//   constructor(amount, currency) {
+//     this._amount = amount;
+//     this._currency = currency;
+//   }
 
+//   get amount() {
+//     return this._amount;
+//   }
+
+//   set amount(newAmount) {
+//     this._amount = newAmount;
+//   }
+
+//   get currency() {
+//     return this._currency;
+//   }
+
+//   set currency(newCurrency) {
+//     this._currency = newCurrency;
+//   }
+
+//   displayFullPrice() {
+//     return `${this._amount} ${this._currency.displayFullCurrency()}`;
+//   }
+
+//   convertPrice(amount, conversionRate) {
+//     return amount * conversionRate;
+//   }
+// }
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
@@ -25,7 +53,8 @@ export default class Pricing {
   displayFullPrice() {
     return `${this._amount} ${this._currency.displayFullCurrency()}`;
   }
-  convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
+
+  convertPrice(conversionRate) {
+    return this._amount * conversionRate;
   }
 }
