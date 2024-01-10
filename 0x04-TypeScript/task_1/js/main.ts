76f5a193b6116550b34cc8c3f60ed6f6ef3cf1eb
Delight Sheriff <delightsheriff@gmail.com>
2024-01-10 23:26:12 +0100

    1. Let's build a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,9 @@
+interface Teacher {
+  readonly firstName: string;
+  fullTimeEmployee: boolean;
+  readonly lastName: string;
+  location: string;
+  contract: boolean;
+  yearsOfExperience?: number;
+  [propName: string]: any;
+}
